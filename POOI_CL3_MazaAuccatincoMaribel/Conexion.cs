8618bf5e09eac6a9a32faa0932b22e8363cc1f7c
Maribel Maza <80657076+mysticBel@users.noreply.github.com>
2022-10-23 21:49:36 -0500

    Agregar archivos de proyecto.

---
+++ POOI_CL3_MazaAuccatincoMaribel/Conexion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace POOI_CL3_MazaAuccatincoMaribel
+{
+    public class Conexion
+    {
+        //Metodo de conexion 
+        public SqlConnection getConecta()
+        {
+            SqlConnection cn = new SqlConnection(ConfigurationManager
+                                          .ConnectionStrings["cn"].ConnectionString);
+            return cn;
+        }
+    }
+}
